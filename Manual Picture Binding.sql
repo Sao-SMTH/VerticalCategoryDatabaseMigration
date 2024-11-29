5672ff0ae059d484b65f419dee9e5229ec52e5ea
Sao Bran - SMTH <sao@smthgoodco.com>
2024-11-29 08:30:34 +0630

    update picture binding

---
+++ Manual Picture Binding.sql
@@ -0,0 +1,11 @@
+Update Categories 
+set VirtualPath='https://smthgoodcategory.s3.ap-southeast-1.amazonaws.com/kids.webp' 
+where Name='Kidswear';
+
+Update Categories 
+set VirtualPath='https://smthgoodcategory.s3.ap-southeast-1.amazonaws.com/lifestyle.webp' 
+where Name='LifeStyle';
+
+Update Categories 
+set VirtualPath='https://smthgoodcategory.s3.ap-southeast-1.amazonaws.com/women.webp' 
+where Name='Women';
