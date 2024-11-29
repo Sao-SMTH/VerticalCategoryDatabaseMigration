1999a1cdcf8336ecd18e9bcd190416f42ccc378c
Sao Bran - SMTH <sao@smthgoodco.com>
2024-11-29 09:49:18 +0630

    feat: update for kids image binding

--- Manual Picture Binding.sql
+++ Manual Picture Binding.sql
@@ -1,11 +1,30 @@
 Update Categories 
-set VirtualPath='https://smthgoodcategory.s3.ap-southeast-1.amazonaws.com/kids.webp' 
+set VirtualPath='https://smthgoodcategory.s3.ap-southeast-1.amazonaws.com/kids.webp',
+VirtualPathThumbnail ='https://smthgoodcategory.s3.ap-southeast-1.amazonaws.com/kids.webp'
 where Name='Kidswear';
 
 Update Categories 
-set VirtualPath='https://smthgoodcategory.s3.ap-southeast-1.amazonaws.com/lifestyle.webp' 
+set VirtualPath='https://smthgoodcategory.s3.ap-southeast-1.amazonaws.com/lifestyle.webp',
+VirtualPathThumbnail='https://smthgoodcategory.s3.ap-southeast-1.amazonaws.com/lifestyle.webp' 
 where Name='LifeStyle';
 
 Update Categories 
-set VirtualPath='https://smthgoodcategory.s3.ap-southeast-1.amazonaws.com/women.webp' 
+set VirtualPath='https://smthgoodcategory.s3.ap-southeast-1.amazonaws.com/women.webp',
+VirtualPathThumbnail='https://smthgoodcategory.s3.ap-southeast-1.amazonaws.com/women.webp' 
 where Name='Women';
+
+Update Categories 
+set VirtualPath='https://s3.ap-southeast-1.amazonaws.com/dev-smthgoodco.com/kids/newborn-landscape.webp',
+VirtualPathThumbnail='https://s3.ap-southeast-1.amazonaws.com/dev-smthgoodco.com/kids/newborn-landscape.webp' 
+where Name='Baby (<3Y)';
+
+Update Categories 
+set VirtualPath='https://s3.ap-southeast-1.amazonaws.com/dev-smthgoodco.com/kids/shop-boys-landscape.webp',
+VirtualPathThumbnail='https://s3.ap-southeast-1.amazonaws.com/dev-smthgoodco.com/kids/shop-boys-landscape.webp' 
+where Name='Boys (>3Y)';
+
+Update Categories 
+set VirtualPath='https://s3.ap-southeast-1.amazonaws.com/dev-smthgoodco.com/kids/shop-girls-landscape.webp',
+VirtualPathThumbnail='https://s3.ap-southeast-1.amazonaws.com/dev-smthgoodco.com/kids/shop-girls-landscape.webp' 
+where Name='Girls (>3Y)';
+
