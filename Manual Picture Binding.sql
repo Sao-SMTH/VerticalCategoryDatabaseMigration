b3520da73401ac8ae862ee6742acec92b0929d42
Sao Bran - SMTH <sao@smthgoodco.com>
2024-12-04 09:36:45 +0630

    feat: update manual databinding for images kids

--- Manual Picture Binding.sql
+++ Manual Picture Binding.sql
@@ -13,6 +13,8 @@ set VirtualPath='https://smthgoodcategory.s3.ap-southeast-1.amazonaws.com/women.
 VirtualPathThumbnail='https://smthgoodcategory.s3.ap-southeast-1.amazonaws.com/women.webp' 
 where Name='Women';
 
+
+
 Update Categories 
 set VirtualPath='https://s3.ap-southeast-1.amazonaws.com/dev-smthgoodco.com/kids/newborn-landscape.webp',
 VirtualPathThumbnail='https://s3.ap-southeast-1.amazonaws.com/dev-smthgoodco.com/kids/newborn-landscape.webp' 
@@ -28,3 +30,57 @@ set VirtualPath='https://s3.ap-southeast-1.amazonaws.com/dev-smthgoodco.com/kids
 VirtualPathThumbnail='https://s3.ap-southeast-1.amazonaws.com/dev-smthgoodco.com/kids/shop-girls-landscape.webp' 
 where Name='Girls (>3Y)';
 
+UPDATE Categories Set VirtualPath='https://s3.ap-southeast-1.amazonaws.com/smthgoodcategory/kids/newborn-landscape.webp',
+VirtualPathThumbnail='https://s3.ap-southeast-1.amazonaws.com/smthgoodcategory/kids/newborn-landscape.webp'
+where ParentId in (
+   Select Id
+    from Categories
+    where ParentId in (
+        Select Id
+    from Categories
+    where Name='Kidswear') 
+) and Name='Tops';
+
+UPDATE Categories Set VirtualPath='https://s3.ap-southeast-1.amazonaws.com/smthgoodcategory/kids/shop-boys-landscape.webp',
+VirtualPathThumbnail='https://s3.ap-southeast-1.amazonaws.com/smthgoodcategory/kids/shop-boys-landscape.webp'
+where ParentId in (
+   Select Id
+    from Categories
+    where ParentId in (
+        Select Id
+    from Categories
+    where Name='Kidswear') 
+) and Name='Bottoms';
+
+UPDATE Categories Set VirtualPath='https://s3.ap-southeast-1.amazonaws.com/smthgoodcategory/kids/shop-girls-landscape.webp',
+VirtualPathThumbnail='https://s3.ap-southeast-1.amazonaws.com/smthgoodcategory/kids/shop-girls-landscape.webp'
+where ParentId in (
+   Select Id
+    from Categories
+    where ParentId in (
+        Select Id
+    from Categories
+    where Name='Kidswear') 
+) and Name='Dresses & One-piece';
+
+UPDATE Categories Set VirtualPath='https://s3.ap-southeast-1.amazonaws.com/smthgoodcategory/kids/kids-accesories-landscape.webp',
+VirtualPathThumbnail='https://s3.ap-southeast-1.amazonaws.com/smthgoodcategory/kids/kids-accesories-landscape.webp'
+where ParentId in (
+   Select Id
+    from Categories
+    where ParentId in (
+        Select Id
+    from Categories
+    where Name='Kidswear') 
+) and Name='Accessories';
+
+UPDATE Categories Set VirtualPath='https://s3.ap-southeast-1.amazonaws.com/smthgoodcategory/kids/newborn-landscape.webp',
+VirtualPathThumbnail='https://s3.ap-southeast-1.amazonaws.com/smthgoodcategory/kids/newborn-landscape.webp'
+where ParentId in (
+   Select Id
+    from Categories
+    where ParentId in (
+        Select Id
+    from Categories
+    where Name='Kidswear') 
+) and Name='Swim';
